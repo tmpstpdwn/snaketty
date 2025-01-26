1842d8934b96305ad8eb937381e7e5ad21196797
tmpstpdwn <tmpstpdwn@proton.me>
2025-01-26 12:07:54 +0530

    cleaned up

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,49 @@
-// IMPORTS
+// Copyright (C) 2025 github.com/tmpstpdwn
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+
+// IMPORTS.
 use std::io::Write;
 use std::{thread, time};
 use k_board::{keyboard::Keyboard, keys::Keys};
-use termion::terminal_size;
+use termion::{terminal_size, cursor};
 use rand::Rng;
 
-// Pair Struct
+// Consts.
+const FPS: u64 = 30;
+const SNAKE_BLOCK: char = '■';
+const FOOD_BLOCK: char = '●';
+const SCREEN_DIAMENTION_PERC: f32 = 0.7;
+const MIN_WIDTH: u32 = 50;
+const MIN_HEIGHT: u32 = 15;
+
+// Pair Struct.
 #[derive(Clone, Copy)]
 struct Pair {
     x: i32,
     y: i32,
 } 
 
-// Object Struct
-#[derive(Clone, Copy)]
+// Object Struct.
 struct Object {
     pos: Pair,
     dir: Pair,
     chr: char,
 }
 
-// Screen Struct
+// Screen Struct.
 struct Screen {
     width: u32,
     height: u32,
@@ -33,27 +56,38 @@ struct Screen {
 impl Screen {
 
     // New.
-    fn new() -> Self {
-        // Get terminal size using termion
-        let (term_width, term_height) = terminal_size().unwrap_or((80, 24)); // Fallback to (80, 24) if unable to get size
+    fn new() -> Option<Self> {
 
-        // Calculate 80% of terminal width and height
-        let width = (term_width as f32 * 0.8) as u32;
-        let height = (term_height as f32 * 0.8) as u32;
+        // Get terminal size using termion.
+        let (term_width, term_height) = match terminal_size() {
+            Ok(size) => size,
+            Err(_) => return None,
+        };
 
-        // Calculate padding to center the screen
+        // Calculate % of terminal width and height.
+        let width = (term_width as f32 * SCREEN_DIAMENTION_PERC) as u32;
+        let height = (term_height as f32 * SCREEN_DIAMENTION_PERC) as u32;
+
+        if width < MIN_WIDTH || height < MIN_HEIGHT {
+            return None
+        }
+
+        // Calculate padding to center the screen.
         let padx = (term_width as u32 - (width + 2)) / 2;
         let pady = (term_height as u32 - (height + 2)) / 2;
 
-        // Create and return a new Screen instance
-        Screen {
-            width,
-            height,
-            padx,
-            pady,
-            header: String::new(),
-            footer: String::new(),
-        }
+        // Create and return a new Screen instance.
+        Some (
+            Screen {
+                width,
+                height,
+                padx,
+                pady,
+                header: String::new(),
+                footer: String::new(),
+            }
+        )
+        
     }
 
     // Clear screen fn.
@@ -62,7 +96,7 @@ impl Screen {
     }
 
     // Render fn.
-    fn render(&self, objects: &Vec<Object>) {
+    fn render(&self, objects: &[&Object]) {
 
         // O/P String.
         let mut output: String = String::new();
@@ -71,19 +105,22 @@ impl Screen {
         output.push_str(&"\n".repeat(self.pady as usize));
 
         // Header.
-        let header_padding = (self.width as usize - self.header.len()) / 2;
-        let remaining_padding = self.width as usize - (header_padding + self.header.len());
-        output.push_str(&" ".repeat(self.padx as usize));
+        output.push_str(&" ".repeat(self.padx as usize)); // Padx.
         output.push_str("┌"); 
-        output.push_str(&"─".repeat(header_padding));
-        output.push_str(&self.header);
-        output.push_str(&"─".repeat(remaining_padding));
+        if self.header.len() < self.width as usize{
+            let header_padding = (self.width as usize - self.header.len()) / 2;
+            let remaining_padding = self.width as usize - (header_padding + self.header.len());
+            output.push_str(&"─".repeat(header_padding));
+            output.push_str(&self.header);
+            output.push_str(&"─".repeat(remaining_padding));
+        } else {
+            output.push_str(&"─".repeat(self.width as usize));
+        }
         output.push_str("┐\n");
 
         // Screen.
         for y in 0..self.height {
-
-            output.push_str(&" ".repeat(self.padx as usize));
+            output.push_str(&" ".repeat(self.padx as usize)); // Padx.
             output.push_str("│");
             for x in 0..self.width {
                 let mut printed = false;
@@ -102,18 +139,24 @@ impl Screen {
         }
 
         // Footer.
-        let footer_padding = (self.width as usize - self.footer.len()) / 2;
-        let remaining_padding = self.width as usize - (footer_padding + self.footer.len());
-        output.push_str(&" ".repeat(self.padx as usize));
+        output.push_str(&" ".repeat(self.padx as usize)); // Padx.
         output.push_str("└");
-        output.push_str(&"─".repeat(footer_padding));
-        output.push_str(&self.footer);
-        output.push_str(&"─".repeat(remaining_padding));
+        if self.footer.len() < self.width as usize{
+            let footer_padding = (self.width as usize - self.footer.len()) / 2;
+            let remaining_padding = self.width as usize - (footer_padding + self.footer.len());
+            output.push_str(&"─".repeat(footer_padding));
+            output.push_str(&self.footer);
+            output.push_str(&"─".repeat(remaining_padding));
+        } else {
+            output.push_str(&"─".repeat(self.width as usize));
+        }
         output.push_str("┘\n");
 
-        // pady.
+
+        // Pady.
         output.push_str(&"\n".repeat(self.pady as usize));
 
+        // Render.
         Screen::clear_screen();
         print!("{}", output);
         std::io::stdout().flush().unwrap();
@@ -122,13 +165,16 @@ impl Screen {
 
 // Main fn.
 fn main() {
+    print!("{}", cursor::Hide);
 
-    let mut screen = Screen::new();
+    let mut screen = Screen::new().unwrap_or_else(|| {
+        eprintln!("Failed to create screen: invalid terminal size");
+        std::process::exit(1);
+    });
 
     screen.header = String::from(" SNAKE-TTY ");
 
-    let fps: u64 = 60;
-    let sleep_duration: time::Duration = time::Duration::from_millis(1000 / fps); 
+    let sleep_duration: time::Duration = time::Duration::from_millis(1000 / FPS); 
     let mut rng = rand::thread_rng();
 
     let mut score: u32 = 0;
@@ -137,24 +183,21 @@ fn main() {
         Object {
             pos: Pair { x: (rng.gen_range(0..screen.width) as i32), y: (rng.gen_range(0..screen.height) as i32)},
             dir: Pair { x: 1, y: 0},
-            chr: 'X'
+            chr: SNAKE_BLOCK
         }
     ]; 
 
     let mut food: Object = Object {
         pos: Pair { x: (rng.gen_range(0..screen.width) as i32), y: (rng.gen_range(0..screen.height) as i32)},
         dir: Pair { x: 1, y: 0},
-        chr: 'O'
+        chr: FOOD_BLOCK
     };
 
-    let mut first_hit: bool = false;
+    let mut first_hit: bool = true;
     let mut is_running: bool = true;
     let mut game_is_on: bool = false;
 
     while is_running {
-
-        // Objects to be rendered.
-        let mut objects: Vec<Object> = vec![];
 
         if game_is_on {
 
@@ -167,48 +210,25 @@ fn main() {
             }
 
             // Head movement.
-            let snake_head: &mut Object = &mut snake[0];
-
             for key in Keyboard::new() {
-                match key {
-                    Keys::Escape => {
-                        is_running = false;
-                        break;
-                    },
-                    Keys::Up if snake_head.dir.y == 0 => {
-                        snake_head.dir = Pair { x: 0, y: -1 };
-                    }
-                    Keys::Down if snake_head.dir.y == 0 => {
-                        snake_head.dir = Pair { x: 0, y: 1 };
-                    }
-                    Keys::Left if snake_head.dir.x == 0 => {
-                        snake_head.dir = Pair { x: -1, y: 0 };
-                    }
-                    Keys::Right if snake_head.dir.x == 0 => {
-                        snake_head.dir = Pair { x: 1, y: 0 };
-                    }
+                match key { Keys::Escape => is_running = false,
+                    Keys::Up if snake[0].dir.y == 0 => snake[0].dir = Pair { x: 0, y: -1 },
+                    Keys::Down if snake[0].dir.y == 0 => snake[0].dir = Pair { x: 0, y: 1 },
+                    Keys::Left if snake[0].dir.x == 0 => snake[0].dir = Pair { x: -1, y: 0 },
+                    Keys::Right if snake[0].dir.x == 0 => snake[0].dir = Pair { x: 1, y: 0 },
                     _ => break,
                 }
             }
 
-            snake_head.pos.x += snake_head.dir.x;
-            snake_head.pos.y += snake_head.dir.y;
+            snake[0].pos.x += snake[0].dir.x;
+            snake[0].pos.y += snake[0].dir.y;
 
             // Screen border teleportation.
-            snake_head.pos.x = (snake_head.pos.x + screen.width as i32) % screen.width as i32;
-            snake_head.pos.y = (snake_head.pos.y + screen.height as i32) % screen.height as i32;
-
-            // Collition with self.
-            let snake_head: Object = snake_head.clone();
-            for snake_block in &snake[1..] {
-                if snake_block.pos.x == snake_head.pos.x && snake_block.pos.y == snake_head.pos.y {
-                    game_is_on = false;
-                    break;
-                }
-            }
+            snake[0].pos.x = (snake[0].pos.x + screen.width as i32) % screen.width as i32;
+            snake[0].pos.y = (snake[0].pos.y + screen.height as i32) % screen.height as i32;
 
             // Collition with food.
-            if food.pos.x == snake_head.pos.x && food.pos.y == snake_head.pos.y {
+            if food.pos.x == snake[0].pos.x && food.pos.y == snake[0].pos.y {
 
                 // Change food coord.
                 loop {
@@ -234,9 +254,17 @@ fn main() {
                     Object {
                         pos: new_pos,
                         dir: tail.dir,
-                        chr: 'X'
+                        chr: SNAKE_BLOCK
                     }
                 );
+            }
+
+            // Collition with self.
+            for snake_block in &snake[1..] {
+                if snake_block.pos.x == snake[0].pos.x && snake_block.pos.y == snake[0].pos.y {
+                    game_is_on = false;
+                    break;
+                }
             }
 
             if score == 0 {
@@ -247,47 +275,46 @@ fn main() {
 
 
         } else {
-            screen.footer = String::from(" Space to start || Escape to quit. ");
             for key in Keyboard::new() {
                 match key {
-                    Keys::Escape => {
-                        is_running = false;
-                        break;
-                    },
+                    Keys::Escape => is_running = false,
                     Keys::Space => {
                         game_is_on = true;
-                        if first_hit {
+                        if !first_hit {
                             score = 0;
                             food.pos.x = rng.gen_range(0..screen.width) as i32;
                             food.pos.y = rng.gen_range(0..screen.height) as i32;
                             snake.clear();
                             snake.push(
                                 Object {
-                                    pos: Pair { x: (rng.gen_range(0..screen.width) as i32), y: (rng.gen_range(0..screen.height) as i32)},
+                                    pos: Pair { 
+                                        x: (rng.gen_range(0..screen.width) as i32), 
+                                        y: (rng.gen_range(0..screen.height) as i32)
+                                    },
                                     dir: Pair { x: 1, y: 0},
-                                    chr: 'X'
+                                    chr: SNAKE_BLOCK
                                 }
                             );
                         } else {
-                            first_hit = true;
+                            first_hit = false;
                         }
-                        break;
                     }
                     _ => break,
                 }
             }
+            screen.footer = String::from(" Space to start || Escape to quit. ");
         }
 
         // Render.
-        for snake_block in &snake {
-            objects.push(snake_block.clone());
-        }
-        objects.push(food.clone());
+        let mut objects: Vec<&Object> = Vec::with_capacity(snake.len() + 1);
+        objects.extend(snake.iter());
+        objects.push(&food);
         screen.render(&objects);
 
         // Limit FPS.
         thread::sleep(sleep_duration);
     }
+    print!("{}", cursor::Show);
 }
 
 // END
